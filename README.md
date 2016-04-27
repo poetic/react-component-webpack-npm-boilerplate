ef07e785526b0149fc979da116d1acde93ce682a
Chun Yang <yang2007chun@gmail.com>
2016-04-27 11:55:10 -0500

    add script

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # react-component-webpack-npm-boilerplate
+
+```
+// START THE TESTING SERVER
+npm install
+npm run browser
+
+// BUILD
+npm run build
+```
--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 import MyComponent from '../src/index'
 import $ from 'jquery'
 
-describe('GooglePlaces', function() {
+describe('MyComponent', function() {
   it('should render', function() {
     chai.assert(true);
   });
